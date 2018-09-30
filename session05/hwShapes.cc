c5418d92c4ebfc758155844d73dff28e3b4c988d
Dov Kruger <Dov.Kruger@gmail.com>
2018-09-30 18:32:26 -0400

    shapes homework

--- session05/hwShapes.cc
+++ session05/hwShapes.cc
@@ -1,22 +1,12 @@
+#include <iostream>
+using namespace std;
 int main() {
-	// Rect(x,y, width, height)
-	Rect r1(100,50, 20, 40);
-	
-	// Circle(x,y,radius)
-	Circle c1(400, 300, 50);
-
-	Line L1(0,0,  300, 400);
-
-#if 0
-	// next week
-	// Heart(systolic, diastolic, bpm)
-	Heart h(120, 70, 72);
-
-	// percentage 100 = perfectly healthy
-	Kidney k(100);
-
-//	Person has 1 heart 2 kidneys
-#endif
+	Rect r1(100, 50, 20, 40);	// Rect(x,y, width, height)
+	cout << r1.area() << '\n';
+  Circle c1(400, 300, 50); // Circle(x,y,radius)
+  cout << c1.area() << '\n'; // area should be pi * radius squared
+  Line L1(0,0, 300, 400);
+  cout << L1.area() << '\n'; // area should be 0
 }
 
 
