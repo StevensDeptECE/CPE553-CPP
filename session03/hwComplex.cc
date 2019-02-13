9455ff033f4c55bb5fed4eff7af16a9827d645fa
Dov Kruger <Dov.Kruger@gmail.com>
2019-02-13 15:26:07 -0500

    better defined hwComplex.cc homework problem

--- session03/hwComplex.cc
+++ session03/hwComplex.cc
@@ -11,12 +11,12 @@
 int main() {
 	const Complex a(1.0, 2.0); // create a complex number with real=1.0 imag=2.0
 	const Complex b(0.5, 0.0); // real = 0.5, imag = 0.0
-	const Complex c = a + b;   // overload operator + using the first syntax we discussed
+	const Complex c = a + b;   // overload operator + using a friend function
 	c.print(); // should print the following to cout: (1.5,2.0)
 	// (member syntax).  Should look the same as method add where the name add
 	// is replaced by operator +
 	const Complex d = a.add(b); // this should be the same as the above except the name
 	d.print();
-	Complex e = -d;
+	Complex e = -d; // implement unary - using a member operator. Since it has one parameter which is this should have ZERO PARAMETERS!!!
 	e.print();
 }
