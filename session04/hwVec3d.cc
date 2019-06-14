913baa3f019d550fe62a6d46df08f9ca048fb968
SeanHartwell <39306634+SeanHartwell@users.noreply.github.com>
2019-06-14 17:48:40 -0400

    Update hwVec3d.cc

--- session04/hwVec3d.cc
+++ session04/hwVec3d.cc
@@ -1,38 +1,35 @@
-double  dot(Vec3d a, Vec3d b) {
-
-}
-
 int main() {
-	// use friend operators
+	// Main() Section 1
 	const Vec3d a(1.0,2.5,3.5); // double precision!
 	const Vec3d b(2.0);         //(2,0,0)
 	const Vec3d c(2.0,3.5);     //(2,3.5,0)
-	const Vec3d e;              //(0,0,0)
+	const Vec3d d;              //(0,0,0)
 
-	const Vec3d f = a + e;
-	const Vec3d g = b - d; // d is not an object, pick a different one
-	double x = dot(f, g); // regular function  f.x*g.x + f.y*g.y + f.z*g.z
-	//	double y = Vec3d::dot(f,g); // static function
-	double z = f.dot(g); // method
-	const Vec3d h = -b;
-
-	double dist = f.mag(); // square root of sum of square
-	double dist2 = f.magSq(); // sum of square
-	double dist3 = f.dist(g); // sqrt or sum of square of diff
-	//dx = f.x-g.x  dy = f.y - g.y      dx*dx + dy*dy...
-	cout << a << '\n';
-	cout << b << '\n';
-	cout << c << '\n';
-	cout << e << '\n';
-	cout << f << '\n';
-	cout << g << '\n';
-	cout << h << '\n';
-	cout << dist << '\n';
-	cout << dist2 << '\n';
-	cout << dist2 << '\n';
-}
-
+	// Main() Section 2
+	const Vec3d e = a + d;
+	const Vec3d f = c - b;
+	const Vec3d g = -e;
 	
+	// Main() Section 3	
+	double r = dot(e, f); // regular function  e.x*f.x + e.y*f.y + e.z*f.z
+	double s = e.dot(f); // method
 
-	
-	
+	// Main() Section 4	
+	double x = e.mag(); // square root of sum of square
+	double y = e.magSq(); // sum of square
+	double z = e.dist(f); // sqrt or sum of square of diff
+
+	// Main() Section 5	
+	cout << "a: " << a << '\n';
+	cout << "b: " << b << '\n';
+	cout << "c: " << c << '\n';
+	cout << "d: " << d << '\n';
+	cout << "e: " << e << '\n';
+	cout << "f: " << f << '\n';
+	cout << "g: " << g << '\n';
+	cout << "r: " << r << '\n';
+	cout << "s: " << s << '\n';
+	cout << "x: " << x << '\n';
+	cout << "y: " << y << '\n';
+	cout << "z: " << z << '\n';
+}	
