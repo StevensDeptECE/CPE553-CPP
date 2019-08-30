1ed115a72c7a6f4088a86c762950e465c79a6b4b
Dov Kruger <Dov.Kruger@stevens.edu>
2019-08-30 13:43:33 -0400

    renamed Session01 to session01 and and typed new examples for 2019F term

---
+++ session01/06sumandprod.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int sum = 0;
+	for(int i = 1; i <= 100; i++) {
+		sum += i; // sum = sum + i     x = x * 2 --> x *= 2    y = y / 3  --> y/=3
+	}
+	cout << sum << '\n';
+
+	int prod = 1;
+	for (int i = 1; i <= 5; i++)
+		prod = prod * i;
+	cout << prod << '\n';
+
+}
---
+++ session01/07float.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float f = 1.5f; // 1.234567
+	double d = 1.5; // 1.23456789012345
+	long double ld =  1.345678902345678901234567890L;
+
+	2.5;
+	2.5 + 6.022e+23;
+
+	float f2 = 1.234567e+38;
+	float f3 = 1.23e-38;
+
+	double d2 = 1.23456789012345e+308;
+
+}
