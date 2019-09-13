77867b029f2cdb25cc47b95dd33a67e17e4b65f2
Dov Kruger <Dov.Kruger@stevens.edu>
2019-09-13 14:34:46 -0400

    fixed Person example, array cannot be const!

--- session03/Person.cc
+++ session03/Person.cc
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <cstring>
+using namespace std;
 class Person {
 private:
-	const char firstName[10];
-	const char lastName[20];
-	const char building[15];
+	char firstName[10];
+	char lastName[20];
+	char building[15];
 	int room;
 public:
 	Person(const char firstName[], const char lastName[], const char building[],
 				 int room) :
-		firstName(strncpy(this->firstName, firstName, 10)),
-		lastName(strncpy(this->lastName, lastName, 20)),
-		building(strncpy(this->building, building, 15)),
 		room(room)
 	{
+		strncpy(this->firstName, firstName, 10);
+		strncpy(this->lastName, lastName, 20);
+		strncpy(this->building, building, 15);
 	}
 	void print() const {
     cout << firstName << " " << lastName << " " << building << " " << room;
@@ -23,7 +24,7 @@ public:
 int main() {
 	const	Person p1("Dov", "Kruger", "Burchard", 217);
 	p1.print();
-	cout << '\n'
-	p1.setName("Freddy");
+	cout << '\n';
+	//	p1.setName("Freddy");
 	p1.print();
 }
