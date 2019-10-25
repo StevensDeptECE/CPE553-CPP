62050dc0b34c11ddd9200a616354b233cec23b30
SeanHartwell <39306634+SeanHartwell@users.noreply.github.com>
2019-10-25 18:20:05 -0400

    Update hwBitmap.cc

--- session08_dynamicmemory/hwBitmap.cc
+++ session08_dynamicmemory/hwBitmap.cc
@@ -1,9 +1,13 @@
 int main() {
 	constexpr uint32_t BLACK = 0xFF000000; // black opaque
-	constexpr uint32_t RED =   0xFF0000FF; // red opaque
+	constexpr uint32_t RED = 0xFFFF0000; // red opaque
+	constexpr uint32_t BLUE = 0xFF0000FF; // blue opaque	
+	constexpr uint32_t RED = 0xFFFFFFFF; // white opaque
+	constexpr uint32_t RED = 0xFFFFFF00; // yellow opaque
 
 	//hardcoded size 600 x 400
 	Bitmap b(BLACK);
+	
         b.horizLine(0, 500, 200, RED); // horizontal line from x=0 to x=500, @y = 200
 	b.vertLine(0, 399, 300, RED); // y=0 to y=399 @ x= 300, red vertical line
 	
@@ -13,6 +17,7 @@ int main() {
 	b.line(400,0, 550,300, YELLOW); // draw a line https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
 	
 	b.ellipse(xcenter, ycenter, xdiameter, ydiameter);
+	
 	b.save("bitmap.png");
 	
 	//OPTIONAL FUNCTION, 100pt bonus for implementing Wu's antialiasing
