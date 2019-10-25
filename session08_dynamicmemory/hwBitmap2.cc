dd0e8c66f52ddd2d742115941e77e50d66853ba7
SeanHartwell <39306634+SeanHartwell@users.noreply.github.com>
2019-10-25 18:20:01 -0400

    Update hwBitmap2.cc

--- session08_dynamicmemory/hwBitmap2.cc
+++ session08_dynamicmemory/hwBitmap2.cc
@@ -12,21 +12,28 @@
 
 int main() {
 	constexpr uint32_t BLACK = 0xFF000000; // black opaque
-	constexpr uint32_t RED =   0xFF0000FF; // red opaque
+	constexpr uint32_t RED = 0xFFFF0000; // red opaque
+	constexpr uint32_t BLUE = 0xFF0000FF; // blue opaque	
+	constexpr uint32_t RED = 0xFFFFFFFF; // white opaque
+	constexpr uint32_t RED = 0xFFFFFF00; // yellow opaque
 
-	DynamicBitmap b(1024, 1024, BLACK);
-  b.horizLine(0, 500, 200, RED); // horizontal line from x=0 to x=500, @y = 200
+	//hardcoded size 600 x 400
+	Bitmap b(BLACK);
+	
+        b.horizLine(0, 500, 200, RED); // horizontal line from x=0 to x=500, @y = 200
 	b.vertLine(0, 399, 300, RED); // y=0 to y=399 @ x= 300, red vertical line
+	
 	b.drawRect(200,200, 100,50, BLUE); // blue rectangle, top-left=200,200 w=100 h=50
 	b.fillRect(201,201, 98,48, WHITE); // same but filled
+	
 	b.line(400,0, 550,300, YELLOW); // draw a line https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
 	
-	//optional, 100pt bonus for implementing Wu's antialiasing
-	https://en.wikipedia.org/wiki/Xiaolin_Wu%27s_line_algorithm
-	b.antialiedLine(400,0, 550,300, YELLOW);
-
+	b.ellipse(xcenter, ycenter, xdiameter, ydiameter);
 	
-  b.ellipse(xcenter, ycenter, xdiameter, ydiameter);
-	b.save("bitmap2.png");
+	b.save("bitmap.png");
+	
+	//OPTIONAL FUNCTION, 100pt bonus for implementing Wu's antialiasing
+	//https://en.wikipedia.org/wiki/Xiaolin_Wu%27s_line_algorithm
+	//b.antialiedLine(400,0, 550,300, YELLOW);        
 }
 
