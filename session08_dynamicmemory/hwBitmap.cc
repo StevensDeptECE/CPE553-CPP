03291bb2ddf88874f8e12dac01bfcc67d11c48b2
SeanHartwell <39306634+SeanHartwell@users.noreply.github.com>
2019-10-26 14:21:47 -0400

    Update hwBitmap.cc

--- session08_dynamicmemory/hwBitmap.cc
+++ session08_dynamicmemory/hwBitmap.cc
@@ -1,32 +1,33 @@
 int main() {
+	// FIRST PART (ADD COLORS HERE)
 	constexpr uint32_t BLACK = 0xFF000000; // black opaque
 	constexpr uint32_t RED = 0xFFFF0000; // red opaque
-	constexpr uint32_t BLUE = 0xFF0000FF; // blue opaque	
-	constexpr uint32_t WHITE = 0xFFFFFFFF; // white opaque
-	constexpr uint32_t YELLOW = 0xFFFFFF00; // yellow opaque
 	
+	
+	// SECOND PART (DO NOT EDIT)
 	xcenter = 100;
 	ycenter = 100;
 	xdiameter = 200;
 	ydiameter = 100;
 
-	//hardcoded size 600 x 400
-	Bitmap b(BLACK);
+	Bitmap b(BLACK); // Hardcoded size (600 x 400 pixels)
 	
-        b.horizLine(0, 500, 200, RED); // horizontal line from x=0 to x=500, @y = 200
-	b.vertLine(0, 399, 300, RED); // y=0 to y=399 @ x= 300, red vertical line
+        b.horizLine(0, 500, 200, RED); // Red horizontal line, from x=0 to x=500, at y = 200
+	b.vertLine(0, 399, 300, RED); // Red vertical line, from y=0 to y=399, at x = 300
 	
-	b.drawRect(200,200, 100,50, BLUE); // blue rectangle, top-left=200,200 w=100 h=50
-	b.fillRect(201,201, 98,48, WHITE); // same but filled
+	b.drawRect(200,200, 100,50, BLUE); // Blue rectangle, TOP-LEFT at x=200, y=200. width=100, height=50
+	b.fillRect(201,201, 98,48, WHITE); // White rectangle, same rules as above, but filled with color
 	
-	b.line(400,0, 550,300, YELLOW); // draw a line https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+	b.line(400,0, 550,300, YELLOW); // Line drawn using https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
 	
-	b.ellipse(xcenter, ycenter, xdiameter, ydiameter);
+	b.ellipse(xcenter, ycenter, xdiameter, ydiameter); //Ellipse using specs from above
 	
 	b.save("bitmap.png");
 	
-	//OPTIONAL FUNCTION, 100pt bonus for implementing Wu's antialiasing
+	
+	// THIRD PART - OPTIONAL FUNCTION
+	// 100pt bonus for properly implementing Wu's antialiasing
 	//https://en.wikipedia.org/wiki/Xiaolin_Wu%27s_line_algorithm
-	//b.antialiedLine(400,0, 550,300, YELLOW);        
+	//b.antialiasedLine(400,0, 550,300, YELLOW);        
 }
 
