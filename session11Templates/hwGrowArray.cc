01463b42785eeab2dfc2ab35e3f5e48953eebabe
Dov Kruger <Dov.Kruger@stevens.edu>
2019-11-16 12:42:03 -0500

    added homework spec for growarray using placement syntax

---
+++ session11Templates/hwGrowArray.cc
@@ -0,0 +1,47 @@
+/*
+	Write a GrowArray class that is templated, and that allows you to 
+	allocate memory but not call the constructor for each element.
+	To do this you will have to cast and use placement new syntax.
+
+	See placementnew.cc, GrowArray.cc
+	You are encouraged to write the class from scratch without looking at 
+	my code, once you understand the fundamentals.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class Person {
+private:
+	uint32_t len;
+	char* name;
+public:
+	Person(const char n[]) : len(strlen(n)) {
+		cout << "Person constructor\n";
+		name = new char[len];
+		memcpy(name, n, len);
+	}
+	Person(const Person& orig) : len(strlen(orig.name)){
+		cout << "copying Person\n";
+		name = new char[len];
+		memcpy(name, orig.name, len);
+	}
+	~Person() {
+		cout << "Destructor\n";
+		delete [] name;
+	}
+};
+
+/*
+	You can write your code here
+ */
+
+
+int main() {
+	GrowArray<Person> list(1000000); // should not call constructor for Person!
+	list.add(Person("Test")); // Should call the constructor once and the copy once
+	/*
+		destructor should be called twice for person (once for the temp, and once for the object in the array. If the optimizer somehow can do it, it could be only once. But most important, you must not call the destructor 1000000 times!
+	*/
+}
