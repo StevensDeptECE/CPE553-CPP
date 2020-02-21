714a96b035e3eaba14fe80e11f75138e9c17208a
Dov Kruger <Dov.Kruger@stevens.edu>
2020-02-20 21:05:12 -0500

    fixed bug in fibo2 recursive fibonacci

--- session03/00recursivefunctions.cc
+++ session03/00recursivefunctions.cc
@@ -30,8 +30,8 @@ int count(int n ) {
 
 */
 int fibo(int n) {
-	int a = 1, b = 1, c;
-	for (int i = 0; i < n; i++) {
+	int a = 1, b = 1, c = 1;
+	for (int i = 2; i < n; i++) {
 		c = a + b;
 		a = b;
 		b = c;
