4ca408e1011982ae0c96598fc5b6067456bb3f95
Dov Kruger <Dov.Kruger@stevens.edu>
2021-03-25 21:17:01 -0400

    better solar system instructions

--- session05/hwSolarSystem.cc
+++ session05/hwSolarSystem.cc
@@ -5,11 +5,45 @@ using namespace std;
 // put your Vec3d implementation here
 
 // put your Body implementation here
+class SolarSystem;
+
+class Body {
+private:
+	double mass;
+	Vec3d pos;
+	Vec3d v;
+	Vec3d a;
+public:
+	Body() {}
+	operator >>(istream& s, Body& b) {
+		// read in the body from the file
+		// radius of your orbit = (Perihelion  + Aphelion) /2
+		// pick a random location on your orbit
+		// pick  a random angle 0 to 2*pi search "c++ random uniform double example"
+		// x = r *cos(ang), y = r*sin(ang)
+		//  compute an initial velocity
+		// Look in wikipedia
+		//https://en.wikipedia.org/wiki/Circular_orbit
+		// 	v = sqrt(G*mofthebodyyouareorbniting/r)
+		//special case: all planets orbit the sun. the sun orbits no one
+		// sun initial v = 0
+	}
+
+	static setAccelerations(vector<Body>& bodies) {
+		for (auto b : bodies) {
+
+		}
+	}
+	friend SolarSystem;
+};
 
 class SolarSystem {
 private:
   vector<Body> bodies;
 public:
+	void stepForward() {
+		Body::setAccelerations(bodies);
+	}
 };
 
 int main() {
@@ -25,16 +59,30 @@ int main() {
      in Vec3d.
      For any body m, the accelation due to another body m2 is:
 
-     a = Gm2/rsq,   rsq = (pos2 - pos).magsq()
+		 sun = (0,0,0)
+     earth = (3,2,0)
+
+		 delta = (sun-earth) = (-3,-2,0)
+
+     dpos = pos2 - pos
+     rsq = dpos.magsq()   -3*-3+-2*-2+0*0 = 13
+     r = sqrt(rsq)        r = magnitude of dpos, the distance to the other body
+
      G = universal gravitational constant 6.667E-11
 
+     F = G*me*m2/rsq  
+		 a = F/me so mass of the earth cancels threfore:
+
+     a = G*m2/rsq  
+
      To calculate the vector acceleration, you must break down a into components
-     dpos = (pos2 - pos).normalize 
+     dirtootherbody = dpos.normalize() 
+		 Acceltowardotherbody = (a*dirtootherbody.x, a*dirtootherbody.y, 0)
      (compute a unit vector in the direction of body2)
 
+		 or in scalars:
      ax = a * dpos.x/r, ay = a * dpos.y/r, az = a * dpos.z/r
 
-     r = magnitude of dpos.
 
 		 If you can do this calculation within vector, it is a generically useful
 		 one but you can also just compute it in Body if you want.
@@ -59,9 +107,6 @@ int main() {
     one body is in a sharply changing orbit, dt may have to be reduced.
     For the above to work you will have to implement some operators in Vec3d.
 
-
-    v^2/r = a
-		v = sqrt(ar)
 	*/
   SolarSystem s("solarsystem.dat");
 	cout << s << "=========\n\n\n"; // print the solar system at load time
