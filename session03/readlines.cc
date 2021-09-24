6a221a21da9f6db4974548e0bc934d24bb6e82ee
Dov Kruger <Dov.Kruger@stevens.edu>
2021-09-24 14:21:07 -0400

    added robot simulation coonfig fie, and examples including s02_buildinglists.cc for the ops you need

--- session03/readlines.cc
+++ session03/readlines.cc
@@ -4,19 +4,15 @@
 using namespace std;
 
 int main() {
-    ifstream f("var.csv");
+    ifstream f("test.txt");
     char buffer[4096]; // make a buffer definitely big enough to hold a line
-    char a[4096]; // only way to be sure each variable cannot overflow
-    char b[4096]; // is to make each one as big as the entire buffer? seem ridiculous
-    char c[4096];
+		char name[20];
+		double mass;
+		double radius;
+
     while (f.getline(buffer, sizeof(buffer))) { // if there is a line
-      isstream line(buffer); // read each line from a string stream attached to the buffer
-      line >> a >> b >> c;
-      // strlen(a)  strlen(b)    strlen(c) "12.345"
-      char name[4];
-      int year = atoi(b);
-      float val = atof(c);
-      line >> name >> year >> val;
-      // do something with the three columns...
+      istringstream line(buffer);
+			line >> name >> mass >> radius;
+			cout  << "name=" << name << " mass=" << mass << " radius=" << radius << '\n';
     }
 }
