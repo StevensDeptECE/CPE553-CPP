5379ef90e26bbcdbfa327b6ded05c95259b6f922
Dov Kruger <Dov.Kruger@stevens.edu>
2022-07-22 10:36:40 -0400

    synch code still not working. asynch io calls callback twice, not sure why

--- threads/conditions2.cc
+++ threads/conditions2.cc
@@ -1,70 +1,81 @@
-#include <iostream>
-#include <atomic>
-#include <condition_variable>
-#include <thread>
-#include <chrono>
-using namespace std;
-using namespace std::chrono_literals;
+	#include <fcntl.h>
+	#include <unistd.h>
+	#include <condition_variable>
+	#include <iostream>
+	#include <thread>
+	using namespace std;
+	using namespace std::chrono_literals;
 
-constexpr uint32_t blockSize = 32768;
-char* buf1;
-char* buf2;
-char* processbuf = buf1;
-char* readbuf = buf2;
+	constexpr uint32_t blockSize = 32768;
+	char* buf1;
+	char* buf2;
+	char* processbuf = buf1;
+	char* readbuf = buf2;
 
-mutex m;
-condition_variable cv;
-int fh;
-int bytesRead;
+	mutex readMutex;
+	mutex procMutex;
+	//condition_variable readCV;
+	//condition_variable procCV;
+	int fh;
+	int bytesRead;
 
-void nextRead(char* buf) {
-	unique_lock<mutex> lk(m);
-	bytesRead = read(fh, readbuf, blockSize);
-	lk.unlock();
-	cv.notify_one();
-}
 
-void process(char* buf) {
-}
+	void nextRead() {
+		while (true) {
+			{
+  	  	unique_lock<mutex> readLock(readMutex);
+				bytesRead = read(fh, readbuf, blockSize);
+				for (int i = 0; i < 5; i++) {
+					cerr << "reading..." << endl;
+					usleep(100000);
+					readLock.unlock();
+				}
+				cerr << "notifying..." << endl;
+			}
+			if (bytesRead != blockSize)  // last time, end here!
+				return;
+			unique_lock<mutex> procLock(procMutex); // wait for process to complete
+		}
+	}
 
-void setup() {
-	buf1 = new char[blockSize];
-	buf2 = new char[blockSize];
-	fh = open("bigfile.dat", O_RDONLY);
-	int currentBytesRead = read(fh, buf1, blockSize);
-	thread reader(nextRead);
-	process(processbuf, currentBytesRead);
-	while (cv.wait(lk, []{return true;})) {
-		cout << "back from wait" << endl;
-		swap(processbuf, readbuf); // switch to other buffer for next time
-		currentBytesRead = bytesRead; // copy locally so thread can do the other one
-		//TODO: is the above a problem? what if
+	void process(char* buf, uint32_t bytesRead) {
+		cout << "processing..." << endl;
+ 		usleep(100000);
+  }
+
+	int thready() {
+		buf1 = new char[blockSize];
+		buf2 = new char[blockSize];
+		fh = open("bigfile.dat", O_RDONLY);
+		if (fh < 0) {
+			throw std::runtime_error("cannot open file!");
+		}
+		int currentBytesRead = read(fh, buf1, blockSize);
+		thread reader(nextRead);
 		process(processbuf, currentBytesRead);
-	}
-  reader.join();
-	
-	delete [] buf1;
-	delete [] buf2;
-}
+		while (true) {
+			{
+  		  unique_lock<mutex> readLock(readMutex);
+			  cout << "back from wait" << endl;
+			  swap(processbuf, readbuf);  // switch to other buffer for next time
+			  currentBytesRead = bytesRead;  // create local copy
+				// TODO: is the above a problem? what if
+			}
+ 		  unique_lock<mutex> procLock(procMutex);
+			process(processbuf, currentBytesRead);
+		}
+		reader.join();
 
-uint32_t waits(condition_variable& cv, char* buf, uint32_t bytesRead) {
-	unique_lock<mutex> lk(cv_m);
-	cv.wait(lk, []{      });	
-}
- 
-void readBlock(condition_variable& cv,
-							 int fh, uint32_t blockSize, char* buf) {
-	int bytesRead = read(fh, buf, blockSize);
-	if (bytesRead > 0) {
-		::bytesRead = bytesRead;
-		cv.notify_all();
+		delete[] buf1;
+		delete[] buf2;
 	}
-}
- 
-int main() {
-	thread t1(waits, 1), t2(waits, 2), t3(waits, 3), t4(signals);
-	t1.join(); 
-	t2.join();
-	t3.join();
-	t4.join();
-}
+
+
+	int main() {
+		try {
+			thready();
+		}catch(std::exception& e) {
+			cerr << e.what() << '\n';
+		}
+		return 0;
+	}
