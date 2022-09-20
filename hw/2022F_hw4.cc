766c6ecf7c160d8d769121adf1a25e67666d301d
Dov Kruger <Dov.Kruger@stevens.edu>
2022-09-20 16:09:24 -0400

    added template for week4 homework, still incomplete

---
+++ hw/2022F_hw4.cc
@@ -0,0 +1,19 @@
+/*
+	Author: 
+	Complete this program by implementing class Complex according to the
+	specification in main. Do not change main.
+	When you are done, delete this comment and add your name as author
+ */
+
+int main() {
+	Complex c1;
+	Complex c2(1.0, 2.5);
+	Complex c3 = 2.5;
+	Complex c4 = c2 + c3;
+	cout << c5 << '\n';
+	Complex c6 = c2 * 2.0;
+	cout << c6 << '\n';
+}
+
+
+		
