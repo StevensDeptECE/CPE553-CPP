e2dba4ae828b7599e78fc183a68221062ed17056
Dov Kruger <Dov.Kruger@stevens.edu>
2022-09-21 10:14:43 -0400

    edited hw4 assignment, renamed directory contrib

--- hw/2022F_hw4.cc
+++ hw/2022F_hw4.cc
@@ -3,6 +3,9 @@
 	Complete this program by implementing class Complex according to the
 	specification in main. Do not change main.
 	When you are done, delete this comment and add your name as author
+
+	The bottom of main uses pi and hence will have to be compiled with
+	g++ -std=c++20 ...
  */
 
 int main() {
@@ -11,9 +14,15 @@ int main() {
 	Complex c3 = 2.5;
 	Complex c4 = c2 + c3;
 	cout << c5 << '\n';
-	Complex c6 = c2 * 2.0;
-	cout << c6 << '\n';
-}
 
+	double x = 50, y = 62.5, r = 10;
+	Circle c(x, y, r);
+	cout << c.contains(x+8,y) << '\n';
+	cout << c.contains(x+10,y) << '\n';
+	cout << c.contains(x+10,y+3) << '\n';
+	cout << c.contains(x,y+10) << '\n';
+	double dx = r * cos(45 * pi / 180);
+	cout << c.contains(x+dx, y+dx) >> '\n';
+}
 
 		
